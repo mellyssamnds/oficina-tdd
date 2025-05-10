2862e283da75d62a281e691edb4cc5b0663cfc09
mellyssamnds <mellyssah.mendes@live.com>
2025-05-10 11:13:32 -0300

    test: implementa testes das operações da calculadora

--- calculadora.test.js
+++ calculadora.test.js
@@ -1,21 +1,30 @@
 const Calculadora = require('./calculadora')
 
 describe('Calculadora', () => {
+  let calculadora
+
+  beforeEach(() => {
+    calculadora = new Calculadora()
+  })
+
   it('Deverá retornar a soma de dois números', () => {
-    //Seu código aqui
-    
+    expect(calculadora.soma(2,2)).toBe(4)
   })
 
   it('Deverá retornar a subtração de dois números', () => {
-    //Seu código aqui
-    
+    expect(calculadora.subtracao(10,2)).toBe(8)
   })
 
   it('Deverá retornar a multiplicação entre dois números', () => {
-    //Seu código aqui
+    expect(calculadora.multiplicacao(5,5)).toBe(25)
   })
 
   it('Deverá retornar a divisao entre dois números', () => {
-    //Seu código aqui
+    expect(calculadora.divisao(10,2)).toBe(5)
   })
+
+  it('Deverá lançar erro ao dividir por zero', () => {
+    expect(() => calculadora.divisao(10, 0)).toThrow("Não permitida divisão por 0!");
+  });
+
 })
