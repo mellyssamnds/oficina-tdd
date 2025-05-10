5d23c438fa39627905b6af6bb0dd3e9eec089752
mellyssamnds <mellyssah.mendes@live.com>
2025-05-10 11:12:10 -0300

    feat: implementa calculadora com as operações básicas

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,22 @@
 class Calculadora {
-  //Seu código aqui
+  soma(a, b) {
+    return a + b;
+  }
+
+  subtracao(a, b) {
+    return a - b;
+  }
+
+  multiplicacao(a, b) {
+    return a * b;
+  }
+
+  divisao(a, b) {
+    if (b === 0) {
+      throw new Error("Não permitida divisão por 0!");
+    }
+    return a / b;
+  }
 } 
 
 module.exports = Calculadora
