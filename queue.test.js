8854764ad53d152a0001275aeb5eb11d838bd8a4
mellyssamnds <mellyssah.mendes@live.com>
2025-05-10 12:18:31 -0300

    test: implementa os testes da função queue

--- queue.test.js
+++ queue.test.js
@@ -1,25 +1,42 @@
 const Queue = require('./queue')
 
 describe('Queue', () => {
-  it('Deve criar uma fila', () => {
-    const queue = new Queue()
+  let queue;
 
-    expect(queue).toEqual({ elements: []})
+  beforeEach(() => {
+    queue = new Queue();
+  })
+
+  it('Deve criar uma fila', () => {
+    expect(queue).toEqual({ elements: []});
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    expect(queue.size()).toEqual(0);
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    queue.enqueue(3);
+    expect(queue.size()).toEqual(1);
+    expect(queue.peek()).toEqual(3);
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    queue.enqueue(1);
+    queue.enqueue(2);
+    expect(queue.peek()).toEqual(1);
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    queue.enqueue(1);
+    queue.enqueue(2);
+    expect(queue.dequeue()).toEqual(1);
+    expect(queue.size()).toEqual(1);
+    expect(queue.peek()).toEqual(2);
   })
+
+  it('Deve retornar Erro ao remover item de uma fila vazia', () => {
+    expect(() => queue.dequeue()).toThrow('Fila vazia');
+  });
+  
 })
